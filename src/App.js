c7161d758483476fc1e62ba638f80cd90d498d7a
BaxanAcharya <baxanacharya@gmail.com>
2020-12-14 20:01:08 +0545

    final projet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import AlanButton from "@alan-ai/alan-sdk-web";
+import NewsCard from "./components/newsCards/NewsCards";
+import useStyles from "./style";
+import "./App.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MessengerCustomerChat from "react-messenger-customer-chat";
+const alankey =
+  "ae8cd43e18da8f2fd51c18260925d3352e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+toast.configure();
 
 function App() {
+  const [newsAtricles, setnewsAtricles] = useState([]);
+  const [active, setActive] = useState(0);
+  const classes = useStyles();
+  const remove = () => {
+    setnewsAtricles([]);
+  };
+
+  useEffect(() => {
+    toast.info(
+      'If you are confused, click the mike icon and say "What does this app do?" or "What can i do here" Alan will give you instruction to get started',
+      { position: toast.POSITION.TOP_LEFT, autoClose: false }
+    );
+
+    AlanButton({
+      key: alankey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setnewsAtricles(articles);
+          // setActive(-1);
+        } else if (command === "highlight") {
+          setActive(() => {
+            setActive((prevActive) => prevActive + 1);
+          });
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+          <MessengerCustomerChat
+            pageId="100972251759163"
+            appId="1012992322500389"
+          />
+        </div>
+
+        <div className={classes.logoContainer}>
+          <Link to="/">
+            <img
+              src="https://alan.app/voice/images/previews/preview.jpg"
+              className={classes.alanLogo}
+              alt="logo"
+            />
+          </Link>
+
+          {newsAtricles.length === 0 ? null : (
+            <Button onClick={remove} size="small" color="secondary">
+              Clear news
+            </Button>
+          )}
+        </div>
+        <NewsCard articles={newsAtricles} active={active} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/newsCard/NewsCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import classNames from "classnames";
+import useStyles from "./styles";
+
+function NewsCard({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+  active,
+}) {
+  const classes = useStyles();
+  return (
+    <Card
+      key={i}
+      className={classNames(
+        classes.card
+        // active === i ? classes.activeCard : null
+      )}
+    >
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          className={classes.media}
+          image={
+            urlToImage ||
+            "https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_960_720.jpg"
+          }
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          Learn more
+        </Button>
+        <Typography variant="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default NewsCard;
